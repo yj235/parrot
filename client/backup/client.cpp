2528e52f1a69c4939ea5f94451f99ed55e36ebeb
yj235 <1162358866@qq.com>
2019-04-28 08:24:01 -0700

    forget backup now added

---
+++ client/backup/client.cpp
@@ -0,0 +1,99 @@
+#include "format.h"
+
+#include <iostream>
+#include <string>
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <unistd.h>
+
+using namespace std;
+
+void login(void){
+	string name, password;
+	cin >> name >> password;
+	while(getchar() != '\n'){
+		continue;
+	}
+
+	KVP login_kvp, name_kvp, password_kvp;
+
+	password_kvp.key = "password";
+	password_kvp.value = password;
+
+	name_kvp.key = "name";
+	name_kvp.value = name;
+	name_kvp.next = &password_kvp;
+
+	login_kvp.key = "login";
+	login_kvp.sub = &name_kvp;
+
+	string s;
+	format(s, &login_kvp);
+	cout << s << endl;
+}
+
+void* t_send(void* fd){
+	login();
+	char message[64] = {0};
+	while(true){
+		cout << "send" << endl;
+		memset(message, 0, sizeof(message));
+		fgets(message, sizeof(message), stdin);
+		message[strlen(message) - 1] = '\0';
+		send(*(int*)fd, message, strlen(message), 0);
+	}
+}
+
+void* t_recv(void* fd){
+	char message[64] = {0};
+	while(true){
+		cout << "recv" << endl;
+		memset(message, 0, sizeof(message));
+		if(!recv(*(int*)fd, message, sizeof(message), 0)){
+			break;
+		}
+		printf("%s\n", message);
+		fflush(stdout);
+	}
+}
+
+int main(int argc, char* argv[]){
+	int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+	char* s_ip = "192.168.196.162";
+	short port = 8080;
+
+	struct sockaddr_in s_sockaddr;
+	s_sockaddr.sin_family = AF_INET;
+	s_sockaddr.sin_port = htons(port);
+	s_sockaddr.sin_addr.s_addr = inet_addr(s_ip);
+
+	while(true){
+		if(connect(fd, (struct sockaddr*)&s_sockaddr, sizeof(s_sockaddr))){
+			cout << "connection failed! reconnect in 2s" << endl;
+			sleep(2);
+		} else {
+			cout << "connected" << endl;
+			break;
+		}
+	}
+
+	pthread_t tid1, tid2;
+	pthread_create(&tid1, NULL, t_send, &fd);
+	pthread_create(&tid2, NULL, t_recv, &fd);
+
+	pthread_join(tid1, NULL);
+	pthread_join(tid2, NULL);
+
+	close(fd);
+
+	//while(true){}
+
+	return 0;
+}
---
+++ server/backup/server.cpp
@@ -0,0 +1,91 @@
+#include <mysql/mysql.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+using namespace std;
+
+vector<int> socket_vector;
+map<int, int> id_socket;
+
+void *client_thread(void *_client_sockfd){
+	char buffer[64] = {0};
+	int client_sockfd = *(int*)_client_sockfd;
+	while(1){
+		memset(buffer, 0, sizeof(buffer));
+		if(!recv(client_sockfd, buffer, sizeof(buffer), 0)){
+			break;
+		}
+		printf("%s", buffer);
+		fflush(stdout);
+	}
+}
+
+void* server_thread(void* _client_sockfd){
+	char buffer[32] = {0};
+	while(1){
+		memset(buffer, 0, sizeof(buffer));
+		fgets(buffer, sizeof(buffer), stdin);
+		buffer[strlen(buffer) - 1] = '\0';
+		send(*socket_vector.begin(), buffer, strlen(buffer), 0);
+	}
+}
+
+int main(){
+	unsigned short port = 8080;
+	struct sockaddr_in server_sockaddr;
+
+	int server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	//设置调用closesocket()后，仍可继续重用该socket.(调用closesocket()一般不会立即关闭socket,而经历TIME_WAIT的过程.)
+	bool bReuseaddr = true;
+	setsockopt(server_sockfd,SOL_SOCKET,SO_REUSEADDR,(const char*)&bReuseaddr,sizeof(bReuseaddr));
+
+	memset(&server_sockaddr, 0, sizeof(server_sockaddr));
+	server_sockaddr.sin_family = AF_INET;
+	server_sockaddr.sin_port = htons(port);
+	server_sockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	bind(server_sockfd, (struct sockaddr*)&server_sockaddr, sizeof(struct sockaddr));
+	//!!!
+	listen(server_sockfd, 4);
+
+	//线程分离
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+	pthread_t server_thread_t;
+	pthread_create(&server_thread_t, &attr, server_thread, NULL);
+	while(true){
+		struct sockaddr_in client_sockfdaddr;
+		socklen_t client_sockaddr_len = sizeof(client_sockfdaddr);
+
+		int client_sockfd = accept(server_sockfd, (struct sockaddr*)&client_sockfdaddr, &client_sockaddr_len);
+
+		//添加客户端socket到vector
+		socket_vector.push_back(client_sockfd);
+
+		pthread_t tid;
+		pthread_create(&tid, &attr, client_thread, &client_sockfd);
+	}
+
+	//pthread_join(tid, NULL);
+
+	while(1);
+
+	close(server_sockfd);
+	//close(client_sockfd);
+}
